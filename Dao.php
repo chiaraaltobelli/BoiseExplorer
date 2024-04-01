66a32ad198f883af71f9c500c0faaf491326ee6b
Chiara Altobelli <chiara.j.altobelli@gmail.com>
2024-03-31 20:54:12 -0600

    update to heroku db

--- Dao.php
+++ Dao.php
@@ -9,15 +9,16 @@ use Psr\Log\LogLevel; // Import LogLevel from Psr\Log namespace
 
 class Dao {
     //Heroku:
-    // private $host = "us-cluster-east-01.k8s.cleardb.net";
-    // private $db = "heroku_19905b6cee32fd5";
-    // private $user = "bd27d135b8e3a9";
-    // private $pass = "d7142ae8";
+    private $host = "us-cluster-east-01.k8s.cleardb.net";
+    private $db = "heroku_19905b6cee32fd5";
+    private $user = "bd27d135b8e3a9";
+    private $pass = "d7142ae8";
 
-    private $host = "localhost";
-    private $db = "boiseexplorer";
-    private $user = "caltobelli";
-    private $pass = "Ronin465$";
+    //WAMP
+    // private $host = "localhost";
+    // private $db = "boiseexplorer";
+    // private $user = "caltobelli";
+    // private $pass = "Ronin465$";
     private $logger; // Add logger property
 
     public function __construct() {
