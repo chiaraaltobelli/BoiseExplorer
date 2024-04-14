c3d5d8cbef6b4094484bc62f56248fb1b07e315c
Chiara Altobelli <chiara.j.altobelli@gmail.com>
2024-04-14 13:12:24 -0600

    switch to Heroku db

--- Dao.php
+++ Dao.php
@@ -9,16 +9,17 @@ use Psr\Log\LogLevel;
 
 class Dao {
     //Heroku:
-    // private $host = "us-cluster-east-01.k8s.cleardb.net";
-    // private $db = "heroku_19905b6cee32fd5";
-    // private $user = "bd27d135b8e3a9";
-    // private $pass = "d7142ae8";
+    private $host = "us-cluster-east-01.k8s.cleardb.net";
+    private $db = "heroku_19905b6cee32fd5";
+    private $user = "bd27d135b8e3a9";
+    private $pass = "d7142ae8";
 
     //WAMP
-    private $host = "localhost";
-    private $db = "boiseexplorer";
-    private $user = "caltobelli";
-    private $pass = "Ronin465$";
+    // private $host = "localhost";
+    // private $db = "boiseexplorer";
+    // private $user = "caltobelli";
+    // private $pass = "Ronin465$";
+
     private $logger;
 
     public function __construct() {
